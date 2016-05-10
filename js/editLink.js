06726e3f51c217afde7fe5cdfe27f18d9a198444
George Bina <george@oxygenxml.com>
2016-05-10 12:54:25 +0300

    Move code inside if block.

--- js/editLink.js
+++ js/editLink.js
@@ -1,10 +1,8 @@
 (function() {
-        
-        var link = document.createElement('a');
-        link.textContent = ' [Edit] ';
-        link.target = '_blank';
-        
         if (!document.querySelector('.generated')) {
+            var link = document.createElement('a');
+            link.textContent = ' [Edit] ';
+            link.target = '_blank';
             var htmlURL = window.location.pathname;
             var file = htmlURL.endsWith('.html')? 
                 htmlURL.slice('/dev/'.length, htmlURL.length - '.html'.length) + '.dita' : 
@@ -17,7 +15,6 @@
               '?url=github%3A%2F%2FgetFileContent%2Fdita-ot%2Fdocs%2Fdevelop%2F' +
               encodeURIComponent(file) + 
               '&ditamap=github%3A%2F%2FgetFileContent%2Fdita-ot%2Fdocs%2Fdevelop%2Fuserguide-book.ditamap';
+            document.querySelector('footer > .container').appendChild(link); 
         }
-        
-        document.querySelector('footer > .container').appendChild(link); 
 }());
