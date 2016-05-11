f1bf6876716f39ba780c9c042ac4c96af484cbb9
George Bina <george@oxygenxml.com>
2016-05-11 10:53:39 +0300

    Update the Edit link on click and on mouse over (this will be useful if you want to copy the edit URL to send it to someone). For generated topics we open the map.

--- js/editLink.js
+++ js/editLink.js
@@ -1,23 +1,28 @@
-(function() {
+function updateEditLink() {
         function getEditURL(branch, file) {
             var projectBase = "github%3A%2F%2FgetFileContent%2Fdita-ot%2Fdocs%2F";
             return "https://www.oxygenxml.com/webapp-demo-aws/app/oxygen.html" + 
                 "?url=" + projectBase + branch + "%2F" + encodeURIComponent(file) + 
                 "&ditamap=" + projectBase + branch + "%2F" + "userguide-book.ditamap";    
         }
-
-        if (!document.querySelector('.generated')) {
-            var link = document.createElement('a');
-            link.textContent = ' [Edit] ';
-            link.target = '_blank';
-            var htmlURL = window.location.pathname;
-            var file = htmlURL.endsWith('.html')? 
-                htmlURL.slice('/dev/'.length, htmlURL.length - '.html'.length) + '.dita' : 
-                htmlURL.slice('/dev/'.length, htmlURL.length) + 'index.dita' ;
-            if (file.indexOf('/build-') != -1) {
-                file = file.replace('build-', '');
-            }
-            link.href= getEditURL("develop", file);
-            document.querySelector('footer > .container').appendChild(link); 
+        function getMapEditURL(branch) {
+            return "https://www.oxygenxml.com/webapp-demo-aws/app/oxygen.html" +  
+                "?url=" + "github%3A%2F%2FgetFileContent%2Fdita-ot%2Fdocs%2F" + branch + "%2F" + "userguide-book.ditamap";     
         }
-}());
+        
+        var link = document.getElementById('editLink');
+        if (link) {
+            if (!document.querySelector('.generated')) {
+                var htmlURL = window.location.pathname;
+                var file = htmlURL.endsWith('.html')? 
+                    htmlURL.slice('/dev/'.length, htmlURL.length - '.html'.length) + '.dita' : 
+                    htmlURL.slice('/dev/'.length, htmlURL.length) + 'index.dita' ;
+                if (file.indexOf('/build-') != -1) {
+                    file = file.replace('build-', '');
+                }
+                link.href= getEditURL("develop", file);
+            } else {
+                link.href = getMapEditURL("develop");    
+            }
+        }
+};
