ea7c17ada822064259ae64d935c18c4244200614
Jarno Elovirta <jarno@elovirta.com>
2018-09-09 10:27:34 +0300

    Add plugins page

---
+++ _js/plugins.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', event => {
+  fetch('http://plugins.dita-ot.org/_all.json')
+    .then(response => response.json())
+    .then(json => {
+      const ul = document.createElement('ul')
+      Object.values(json)
+        .filter(plugin => !!plugin)
+        .forEach(plugin => {
+          const li = document.createElement('li')
+          li.appendChild(document.createTextNode(plugin[0].name))
+          ul.appendChild(li)
+        })
+      document.getElementById('plugins').appendChild(ul)
+    })
+    .catch(err => {
+      console.error('Failed to fetch plugins: ' + err)
+    })
+})
